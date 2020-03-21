626fcddabf01051a06a25393ae838f34d3f724e3
jamessspanggg <jamespangmunwai@gmail.com>
2020-03-21 23:07:02 +0800

    fix indentation

--- GithubBotWebService/__main__.py
+++ GithubBotWebService/__main__.py
@@ -10,14 +10,14 @@ routes = web.RouteTableDef()
 router = routing.Router()
 
 @router.register("issues", action="opened")
-    async def issue_opened_event(event, gh, *args, **kwargs):
-        """
-        Whenever an issue is opened, greet the author and say thanks
-        """
-        url = event.data["issue"]["comments_url"]
-        author = event.data["issue"]["user"]["login"]
-        message = f"Thanks for the report @{author}! I will look into it asap! (I'm a bot)."
-        await gh.post(url, data={"body": message})
+async def issue_opened_event(event, gh, *args, **kwargs):
+    """
+    Whenever an issue is opened, greet the author and say thanks
+    """
+    url = event.data["issue"]["comments_url"]
+    author = event.data["issue"]["user"]["login"]
+    message = f"Thanks for the report @{author}! I will look into it asap! (I'm a bot)."
+    await gh.post(url, data={"body": message})
 
 @routes.post("/")
 async def main(request):
